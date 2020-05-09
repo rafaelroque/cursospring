23aa64532cf9507cc4b68ddfd4496bf018b70322
Rafael Roque <rafa834@gmail.com>
2020-05-08 22:10:20 -0300

    Criando instanciacao de Categoria

--- src/main/java/com/example/demo/CursospringApplication.java
+++ src/main/java/com/example/demo/CursospringApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.domain.Categoria;
+import com.example.demo.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+	   Categoria cat1 = new Categoria(null , "Informatica");
+	   Categoria cat2 = new Categoria(null , "Escritorio");
+		
+	   repo.saveAll(Arrays.asList(cat1 , cat2));
+	   
+	}
+
 }
+ 
