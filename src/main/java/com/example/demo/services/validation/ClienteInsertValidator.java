5f2735db639b1c0f2112aff1882fdbf498e5bfed
Rafael Roque <rafa834@gmail.com>
2020-05-14 22:39:24 -0300

    Validação customizada email de cliente

--- src/main/java/com/example/demo/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/example/demo/services/validation/ClienteInsertValidator.java
@@ -6,11 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.domain.Cliente;
 import com.example.demo.domain.enums.TipoCliente;
 import com.example.demo.dto.ClienteNewDTO;
+import com.example.demo.repositories.ClienteRepository;
 import com.example.demo.resources.exception.FieldMessage;
 import com.example.demo.services.validation.utils.BR;
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfCnpj())){
 			list.add(new FieldMessage("cpfCnpj","CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldMessage("email","Email já existe"));
 		}
 
 		for (FieldMessage e : list) {
